70b99c98e79f8f5bcc838cabd76cecc496df101f
Suneel Nerre <suneel.nerre@gmail.com>
2024-06-06 08:32:01 +0530

    Generated md5 hash and fetched data from API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("successfully linked script file with html file!");
+
+const publicKey = "1c0de1d88e61f8ad7b98a8d4bc5b918f";
+const privateKey = "85c8ac848dc84d691b77bf457255ee8ad58f8501";
+const timeStamp = "1717642193559";
+const hashKey = "f23dd071d37f04f324f709bf556fdaf5";
+
+const URL = "https://gateway.marvel.com/v1/public/characters?ts=1717642193559&apikey=1c0de1d88e61f8ad7b98a8d4bc5b918f&hash=f23dd071d37f04f324f709bf556fdaf5&nameStartsWith=thor";
+
+
+async function searchHeros(){
+    let response = await fetch(URL);
+    let data = await response.json();
+    console.log(data.data.results[0]);
+}
+
+// console.log(Date.now());
