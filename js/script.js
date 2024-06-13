1fe9a10b7ab7f64add7b807ff12616c7de1935ab
Suneel Nerre <suneel.nerre@gmail.com>
2024-06-13 07:57:24 +0530

    Show searched results

--- js/script.js
+++ js/script.js
@@ -30,11 +30,58 @@ async function searchHeros(textSearched) {
 
     // API call to get the data
     await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${textSearched}&apikey=1c0de1d88e61f8ad7b98a8d4bc5b918f&hash=f23dd071d37f04f324f709bf556fdaf5&ts=1717642193559`)
-    .then(res => res.json()) // converting response data to JSON format
-    .then(data => showSearchedResults(data.data.results)) // sending searched results to show in HTML
+        .then(res => res.json()) // converting response data to JSON format
+        .then(data => showSearchedResults(data.data.results)) // sending searched results to show in HTML
 }
 
 // function to display searched results in DOM
-function showSearchedResults(searchedHero){
+function showSearchedResults(searchedHero) {
+    let favouritesCharacterIDs = localStorage.getItem("favoriteCharactersIDs");
+    if (favouritesCharacterIDs == null) {
+        favouritesCharacterIDs = new Map();
+    } else if (favouritesCharacterIDs != null) {
+        favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favoriteCharacterIDs")));
+    }
+
+    searchResults.innerHTML = ``;
+    let count = 1;
+
+    for (const key in searchedHero) {
+        if (count <= 5) {
+            let hero = searchedHero[key];
+            searchResults.innerHTML += `
+            <li class="flex-row single-search-result">
+                <div class="flex-row img-info">
+                        <img src="${hero.thumbnail.path + '/portrait_medium.' + hero.thumbnail.extension}" alt="">
+                        <div class="hero-info">
+                            <a class="character-info" href="./more-info.html">
+                                <span class="hero-name">${hero.name}</span>
+                            </a>
+                        </div>
+                </div>
+                <div class="flex-col buttons">
+                        <!-- <button class="btn"><i class="fa-solid fa-circle-info"></i> &nbsp; More Info</button> -->
+                        <button class="btn add-to-fav-btn">${favouritesCharacterIDs.has(`${hero.id}`) ? "<i class=\"fa-solid fa-heart-circle-minus\"></i> &nbsp; Remove from Favourites" : "<i class=\"fa-solid fa-heart fav-icon\"></i> &nbsp; Add to Favourites</button>"}
+                </div>
+                <div style="display:none;">
+                        <span>${hero.name}</span>
+                        <span>${hero.description}</span>
+                        <span>${hero.comics.available}</span>
+                        <span>${hero.series.available}</span>
+                        <span>${hero.stories.available}</span>
+                        <span>${hero.thumbnail.path + '/portrait_uncanny.' + hero.thumbnail.extension}</span>
+                        <span>${hero.id}</span>
+                        <span>${hero.thumbnail.path + '/landscape_incredible.' + hero.thumbnail.extension}</span>
+                        <span>${hero.thumbnail.path + '/standard_fantastic.' + hero.thumbnail.extension}</span>
+                </div>
+            </li>
+            `
+        }
+        count++;
+    }
+    events();
+}
+
+function events(){
     
 }
