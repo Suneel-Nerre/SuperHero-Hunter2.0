7fc8a0f7793f0e8dc6891fd04cd8fcdeec73858d
Suneel Nerre <suneel.nerre@gmail.com>
2024-06-17 16:58:29 +0530

    added styles to info page

--- js/info.js
+++ js/info.js
@@ -12,10 +12,9 @@ window.addEventListener("load", function () {
 
     info.innerHTML = `
         <div class="flex-row hero-name">${heroInfo.name}</div>
-            <div class="flex-row hero-img-and-more-info">
+            <div class="hero-img-and-more-info">
                 <img id="portraitImage" class="hero-img" src="${heroInfo.portraitImage}" alt="">
-                <img style="display:none;" id="landscapeImage" src="${heroInfo.landscapeImage}" alt="">
-                <div class="flex-col more-info">
+                <div class="more-info">
                         <div class="flex-row id">
                             <b>ID:</b><span>${heroInfo.id}</span>
                         </div>
@@ -33,17 +32,6 @@ window.addEventListener("load", function () {
             <div class="flex-col hero-discription">
                 <b>Discription:</b>
                 <p>${heroInfo.description != "" ? heroInfo.description : "No Description Available"}</p>
-            </div>
-            <div style="display:none;">
-                <span>${heroInfo.name}</span>
-                <span>${heroInfo.portraitImage}</span>
-                <span>${heroInfo.landscapeImage}</span>
-                <span>${heroInfo.id}</span>
-                <span>${heroInfo.comics}</span>
-                <span>${heroInfo.series}</span>
-                <span>${heroInfo.stories}</span>
-                <span>${heroInfo.squareImage}</span>
-                <span>${heroInfo.description}</span>
             </div>
             <button class="btn add-to-fav-btn">${favouritesCharacterIDs.has(`${heroInfo.id}`) ? "<i class=\"fa-solid fa-heart-circle-minus\"></i> &nbsp; Remove from Favourites" : "<i class=\"fa-solid fa-heart fav-icon\"></i> &nbsp; Add to Favourites</button>"}
         `
